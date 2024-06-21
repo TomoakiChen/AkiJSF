ff4b6d4c885b472e1c8fb70927b3cea3ac95c178
TomoakiChenSinica <tomoaki.sinica@gmail.com>
2024-06-21 08:53:07 +0800

    新增 switchableTextInput 元件
    初版，算可以用，但有一些issue

---
+++ AkiPrimeFacesUI/src/main/java/tw/dev/tomoaki/jsf/pf/ui/enumoption/TextInputType.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tw.dev.tomoaki.jsf.pf.ui.enumoption;
+
+import tw.dev.tomoaki.enumoption.CodedEnum;
+import tw.dev.tomoaki.enumoption.helper.EnumHelper;
+
+/**
+ *
+ * @author tomoaki
+ */
+public enum TextInputType implements CodedEnum {
+
+    PLAIN_TEXT_AREA("PlainTextArea", "純文字的 TextArea"),
+    TEXT_EDITOR("TextEditor", "Rich Text Editor");
+
+    private String code;
+    private String description;
+
+    private TextInputType(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public static TextInputType codeOf(String desigCode) {
+        return EnumHelper.codeOf(desigCode, TextInputType.class);
+    }
+
+}
